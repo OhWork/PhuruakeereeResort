1cf93c8151862e8a32fe8735999d4205c0833a95
Firore <takbasic722@gmail.com>
2018-05-23 20:57:46 +0700

    เปิดประเด็น

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link rel="stylesheet" href="CSS/bootstrap.css">
+        <link rel="stylesheet" href="CSS/jquery-ui.css">
+        <link rel="stylesheet" href="CSS/main.css">
+		<link rel="stylesheet" href="CSS/scss/_input-group.scss">
+		<title></title>
+	</head>
+	<body>
+		<div class="warpper">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+						<div class="row">
+							<!-- วางสไลท์ -->
+						</div>
+					</div>
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+						<div class="row">
+							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Home</a>
+							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">About</a>
+							<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Accommodations</a>
+							<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Restaurants & Cafe</a>
+							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Events</a>
+							<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Travel Tips</a>
+							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Booking</a>
+							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Payment</a>
+							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Contact</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
