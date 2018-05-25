b7caa38a090b4687fe8e7fa88846740594615ece
Firore <takbasic722@gmail.com>
2018-05-25 21:30:00 +0700

    อัพเดต

--- index.php
+++ index.php
@@ -14,7 +14,16 @@
 				<div class="row">
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 						<div class="row">
-							<!-- วางสไลท์ -->
+							<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="height : 180px;background-color: #00fff0;">
+								<div class="row">
+									<center><p>LOGO</p></center>
+								</div>
+							</div>
+							<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9" style="height : 180px;background-color: #000ff0;">
+								<div class="row">
+									<center><p>SLIDE HEAD ONLY</p></center>
+								</div>
+							</div>
 						</div>
 					</div>
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
@@ -28,6 +37,43 @@
 							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Booking</a>
 							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Payment</a>
 							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Contact</a>
+						</div>
+					</div>
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+						<div class="row">
+							<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+								<div class="row">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background-color: #0fff00;">
+										<div class="row">
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 16px;">
+												<p>ติดต่อจองห้องพัก</p>
+											</div>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<p>Line :</p>
+											</div>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<p>Tel :</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height : 244px;background-color: #00000f;">
+										<div class="row">
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 16px;">
+												<p>Facebook FEED</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">
+								<div class="row">
+									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height : 380px;background-color: #aaaa00;">
+										<div class="row">
+											<p>RESORT PIC</p>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
