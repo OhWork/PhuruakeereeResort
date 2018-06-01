403aca49388420d751c9f4aa53c6e5dd2cb5e2fc
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-06-01 22:34:05 +0700

    อัพวันนี้

--- admin/insert_editdata.php
+++ admin/insert_editdata.php
@@ -6,14 +6,44 @@
 
 	$rs=$db->specifyfytable('MAX(gallery_id)','gallery','datawebsite','gallery_datawebsite_id = datawebsite_id','gallery_datawebsite_id',$id)->executeAssoc();
 // 	print_r($rs['MAX(gallery_id)']);
+	function generateRandomString($length = 10) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$charactersLength = strlen($characters);
+		$randomString = '';
+		for ($i = 0; $i < $length; $i++) {
+			$randomString .= $characters[rand(0, $charactersLength - 1)];
+		}
+		return $randomString;
+	}
 
-	if($a == $rs['MAX(gallery_id)']){
+	$target_dir = '../img/temp/';
+	$target_file = $target_dir.basename($_FILES['image']['name']);
+	$img_new_name = generateRandomString(10);
+	$target_dir_save = '../img/'.$img_new_name.'.jpg';
+	move_uploaded_file($_FILES['image']['name'], $target_dir_save);
+
+	$showidindb = $rs['MAX(gallery_id)'];
+/*
+	if($a == $showidindb){
 		//อัพเดทไปเลยดิ รอไร
 		echo "เท่ากัน";
-		}
 	}else{
-		for
-		echo "เกิน";
+		for($i=0; $i<$showidindb; $i++){
+			echo "จากตัวแปร I";
+			echo "<pre>";
+			print_r($_FILES['image']['name'][$i]);
+			echo "</pre>";
+
+		}
+		$j = $a-$i;
+		for($k=0; $k<$j; $k++){
+		echo "จากตัวแปร K";
+		echo "<pre>";
+		print_r($_FILES['image']['name'][$k]);
+		echo "</pre>";
+
+		}
 	}
+*/
 
 ?>
