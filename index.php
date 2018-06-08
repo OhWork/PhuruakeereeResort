f2f01e861ac706ba5e9727b0a71f3bb20518da3c
Firore <takbasic722@gmail.com>
2018-06-08 22:25:30 +0700

    เพิ่ม

--- index.php
+++ index.php
@@ -14,67 +14,143 @@
 				<div class="row">
 					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 						<div class="row">
-							<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3" style="height : 180px;background-color: #00fff0;">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="border-radius:6px;background-color:#ffffff;margin-bottom:5px;">
 								<div class="row">
-									<center><p>LOGO</p></center>
+									<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+										<div class="row">
+											<img class="col-12" src="img/noimage.png" width="100%" height="100%">
+										</div>
+									</div>
+									<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">
+										<div class="row">
+											<center><p>SLIDE HEAD ONLY</p></center>
+										</div>
+									</div>
 								</div>
 							</div>
-							<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9" style="height : 180px;background-color: #000ff0;">
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 								<div class="row">
-									<center><p>SLIDE HEAD ONLY</p></center>
+									<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Home</a>
+									<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">About</a>
+									<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Accommodations</a>
+									<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Restaurants & Cafe</a>
+									<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Events</a>
+									<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Travel Tips</a>
+									<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Booking</a>
+									<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Payment</a>
+									<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Contact</a>
+								</div>
+							</div>
+							<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="border-radius:6px;background-color:#ffffff;margin-top:5px;">
+								<div class="row">
+									<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+										
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 16px;">
+														<p>ติดต่อจองห้องพัก</p>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<p>Line : @XXXxxXXXX</p>
+													</div>
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+														<p>Tel : xxx-xxx-xxxx</p>
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height : 244px;">
+												<div class="row">
+													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 16px;">
+														<p>Facebook FEED</p>
+													</div>
+												</div>
+											</div>
+										
+									</div>
+									<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9" style="border-left: solid 1px #868e96;">
+										<div class="row">
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+														<div class="row">
+															<img class="col-12" src="img/noimage.png" width="100%" height="200">
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>Roomname</P>
+															</div>
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>price xxxx฿</P>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+														<div class="row">
+															<img class="col-12" src="img/noimage.png" width="100%" height="200">
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>Roomname</P>
+															</div>
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>price xxxx฿</P>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+														<div class="row">
+															<img class="col-12" src="img/noimage.png" width="100%" height="200">
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>Roomname</P>
+															</div>
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>price xxxx฿</P>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+												<div class="row">
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+														<div class="row">
+															<img class="col-12" src="img/noimage.png" width="100%" height="200">
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>Roomname</P>
+															</div>
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>price xxxx฿</P>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+														<div class="row">
+															<img class="col-12" src="img/noimage.png" width="100%" height="200">
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>Roomname</P>
+															</div>
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>price xxxx฿</P>
+															</div>
+														</div>
+													</div>
+													<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-4">
+														<div class="row">
+															<img class="col-12" src="img/noimage.png" width="100%" height="200">
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>Roomname</P>
+															</div>
+															<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:right;">
+																<p>price xxxx฿</P>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-						<div class="row">
-							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Home</a>
-							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">About</a>
-							<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Accommodations</a>
-							<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Restaurants & Cafe</a>
-							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Events</a>
-							<a class="btn btn-secondary col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2" href="#">Travel Tips</a>
-							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Booking</a>
-							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Payment</a>
-							<a class="btn btn-secondary col-xl-1 col-lg-1 col-md-1 col-sm-1 col-1" href="#">Contact</a>
-						</div>
-					</div>
-					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-						<div class="row">
-							<div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
-								<div class="row">
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="background-color: #0fff00;">
-										<div class="row">
-											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 16px;">
-												<p>ติดต่อจองห้องพัก</p>
-											</div>
-											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-												<p>Line :</p>
-											</div>
-											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-												<p>Tel :</p>
-											</div>
-										</div>
-									</div>
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height : 244px;background-color: #00000f;">
-										<div class="row">
-											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 16px;">
-												<p>Facebook FEED</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-xl-9 col-lg-9 col-md-9 col-sm-9 col-9">
-								<div class="row">
-									<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height : 380px;background-color: #aaaa00;">
-										<div class="row">
-											<p>RESORT PIC</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+					<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="text-align:center;margin-top:16px;">
+						<p>copy right by BraBraBra company limited</P>
 					</div>
 				</div>
 			</div>
