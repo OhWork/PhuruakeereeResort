6532bfb6a1a2cf40a230ae6fbf71e9126f15800b
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-06-13 17:07:15 +0700

    อัพการโชว์ข้อมูลจริง

--- admin/showdata.php
+++ admin/showdata.php
@@ -15,27 +15,106 @@
 		}
 	}
 	else if($id == 2 ){
-	echo "Hello World 2";
-	}
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}	}
 	else if($id == 3 ){
-	echo "Hello World 3";
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}
 	}
 	else if($id == 4 ){
-	echo "Hello World 4";
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}
 	}
 	else if($id == 5 ){
-	echo "Hello World 5";
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}
 	}
 	else if($id == 6 ){
-	echo "Hello World 6";
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}
 	}
 	else if($id == 7 ){
-	echo "Hello World 7";
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}
 	}
 	else if($id == 8 ){
-	echo "Hello World 8";
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}
 	}
 	else if($id == 9 ){
-	echo "Hello World 9";
+	?>
+		<h2><?php print_r($rshead['datatype_name']);?></h2>
+		<?php
+		$rs =$db->findbyPK('datawebsite','datatype_datatype_id',$id)->execute();
+		foreach($rs as $showdata){ ?>
+			<div class="jumbotron mt-2">
+			  <h1 class="display-5 "><?php echo $showdata['datawebsite_detail'] ?></h1>
+			  <a class="btn btn-primary btn-lg float-right" href="admin_index.php?url=edit_data.php&id=<?php echo $showdata['datawebsite_id'];?>" role="button">แก้ไข</a>
+			</div>
+		<?php
+		}
 	}
 ?>
