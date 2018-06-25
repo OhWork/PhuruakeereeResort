84eeb76517f3c2642cc23af9cff16545456086ad
Puttikun Wongpanyanurak <puttikun@hotmail.com>
2018-06-25 14:25:38 +0700

    อัพเดท feed facebook

--- index.php
+++ index.php
@@ -51,7 +51,15 @@
 											<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="height : 244px;">
 												<div class="row">
 													<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" style="margin-top: 16px;">
-														<p>Facebook FEED</p>
+														<div id="fb-root"></div>
+														<script>(function(d, s, id) {
+														  var js, fjs = d.getElementsByTagName(s)[0];
+														  if (d.getElementById(id)) return;
+														  js = d.createElement(s); js.id = id;
+														  js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v3.0&appId=236799303381057&autoLogAppEvents=1';
+														  fjs.parentNode.insertBefore(js, fjs);
+														}(document, 'script', 'facebook-jssdk'));</script>
+														<div class="fb-page" data-href="https://www.facebook.com/PhuruaKeereeResort/" data-tabs="timeline" data-height="400" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/PhuruaKeereeResort/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/PhuruaKeereeResort/">ภูเรือคีรีรีสอร์ท PhuruaKeeree Resort</a></blockquote></div>
 													</div>
 												</div>
 											</div>
